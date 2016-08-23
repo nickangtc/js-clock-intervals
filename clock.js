dd7d908512033ebdbd0e7635941faadbd282382f
nickangtc <nickangtc@gmail.com>
2016-08-23 15:28:23 +0800

    Synchronised clock to show actual current time.

--- clock.js
+++ clock.js
@@ -1,57 +1,73 @@
 console.log("is js working?");
 
-setInterval(updateSec, 1000);
-setInterval(updateMin, 60000);
-setInterval(updateHour, 3600000);
+document.addEventListener('DOMContentLoaded', function() {
+  var now = new Date();
+  console.log(now);
+  console.log(now.getMinutes());
 
-var secNow = 1;
-var minNow = 1;
-var hourNow = 1;
+  var secNow = now.getSeconds();
+  var minNow = now.getMinutes();
+  var hourNow = now.getHours();
 
-function updateSec () {
-  var x = secRotation(secNow);
-  var el = document.getElementById("second");
-  el.style.transform = "rotate(" + x + "deg)";
-}
-function secRotation (s) {
-  if (s !== 60) {
-    secNow++;
-    return (s / 60) * 360;  // returns degree of hand
-  }
-  else {
-    secNow = 1;
-    return 1;
-  }
-}
+  updateSec();
+  updateMin();
+  updateHour();
 
-function updateMin () {
-  var x = minRotation(minNow);
-  var el = document.getElementById("minute");
-  el.style.transform = "rotate(" + x + "deg)";
-}
-function minRotation (m) {
-  if (m !== 60) {
-    minNow++;
-    return (m / 60) * 360;  // returns degree of hand
-  }
-  else {
-    minNow = 1;
-    return 1;
-  }
-}
+  setInterval(updateSec, 1000);
+  setInterval(updateMin, 60000);
+  setInterval(updateHour, 3600000);
 
-function updateHour () {
-  var x = hourRotation(hourNow);
-  var el = document.getElementById("hour");
-  el.style.transform = "rotate(" + x + "deg)";
-}
-function hourRotation (h) {
-  if (h !== 60) {
-    hourNow++;
-    return (h / 60) * 360;  // returns degree of hand
+
+  function updateSec () {
+    console.log("inside updateSec function");
+    var x = secRotation(secNow);
+    var el = document.getElementById("second");
+    el.style.transform = "rotate(" + x + "deg)";
   }
-  else {
-    hourNow = 1;
-    return 1;
+  function updateMin () {
+    console.log("inside updateMin function");
+    var x = minRotation(minNow);
+    var el = document.getElementById("minute");
+    el.style.transform = "rotate(" + x + "deg)";
   }
-}
+  function updateHour () {
+    console.log("inside updateHour function");
+    var x = hourRotation(hourNow);
+    var el = document.getElementById("hour");
+    el.style.transform = "rotate(" + x + "deg)";
+  }
+
+  function secRotation (s) {
+    if (s !== 60) {
+      secNow++;
+      return (s / 60) * 360;  // returns degree of hand
+    }
+    else {
+      secNow = 1;
+      return 1;
+    }
+  }
+
+  function minRotation (m) {
+    if (m !== 60) {
+      minNow++;
+      console.log("returning " + ((m / 60) * 360) + " degrees for mins");
+      return (m / 60) * 360;  // returns degree of hand
+    }
+    else {
+      minNow = 1;
+      return 1;
+    }
+  }
+
+  function hourRotation (h) {
+    if (h !== 24) {
+      hourNow++;
+      return (h / 12) * 360;  // returns degree of hand
+    }
+    else {
+      hourNow = 1;
+      return 1;
+    }
+  }
+});
